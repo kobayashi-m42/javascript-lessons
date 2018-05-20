47c3247a973573754ff8c0b47d160215b8c92f39
kobayashi-m42 <m42kobayashi@gmail.com>
2018-05-21 01:50:57 +0900

    ストップボタンの実装。

--- MySlot/js/main.js
+++ MySlot/js/main.js
@@ -8,20 +8,35 @@
     'cherry.png',
   ];
 
+  const timers = [];
+  const panelsLength = panels.length;
+
   /**
    * スロットの実行
    *
    * @param n
    */
-  const runSlot  = (n) => {
-    setTimeout(() => {
+  const runSlot = (n) => {
+    timers[n] = setTimeout(() => {
       panels[n].children[0].src = `img/${cards[Math.floor(Math.random() * cards.length)]}`;
       runSlot(n);
-    },50);
+    }, 50);
   };
 
+  /**
+   * パネルの停止
+   */
+  const stopPanel = () => {
+    for (let i = 0; i < panelsLength; i += 1) {
+      panels[i].children[1].addEventListener('click', (e) => {
+        clearTimeout(timers[e.target.dataset.index]);
+      });
+    }
+  };
+
+  stopPanel();
+
   spin.addEventListener('click', () => {
-    const panelsLength = panels.length
     for (let i = 0; i < panelsLength; i += 1) {
       runSlot(i);
     }
