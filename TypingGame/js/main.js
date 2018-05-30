d2eb5fdb15bccc75d52bee28c6354da12502227d
kobayashi-m42 <m42kobayashi@gmail.com>
2018-05-30 16:34:56 +0900

    タイプされたキーを取得し、スコアをカウントする処理を追加。 正解した文字を置き換える処理を追加。

--- TypingGame/js/main.js
+++ TypingGame/js/main.js
@@ -4,10 +4,33 @@
   const missLabel = document.getElementById('miss');
 
   const currentWord = 'apple';
-  const score = 0;
-  const miss = 0;
+  let currentLocation = 0;
+  let score = 0;
+  let miss = 0;
 
   target.innerText = currentWord;
   scoreLabel.innerText = score;
   missLabel.innerText = miss;
+
+  window.addEventListener('keyup', e => {
+    if (
+      String.fromCharCode(e.keyCode) ===
+      currentWord[currentLocation].toUpperCase()
+    ) {
+      currentLocation += 1;
+
+      let placeholder = '';
+      for (let i = 0; i < currentLocation; i += 1) {
+        placeholder += '_';
+      }
+      target.innerText = `${placeholder}${currentWord.substring(
+        currentLocation
+      )}`;
+      score += 1;
+      scoreLabel.innerText = score;
+    } else {
+      miss += 1;
+      missLabel.innerText = miss;
+    }
+  });
 })();
