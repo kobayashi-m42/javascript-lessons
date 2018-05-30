64ec51c57eeeacf0098990dcfe08f45243a929a9
kobayashi-m42 <m42kobayashi@gmail.com>
2018-05-30 16:48:02 +0900

    ターゲットの単語がランダムに設定されるように修正。

--- TypingGame/js/main.js
+++ TypingGame/js/main.js
@@ -3,14 +3,22 @@
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
 
-  const currentWord = 'apple';
+  let currentWord = 'apple';
   let currentLocation = 0;
   let score = 0;
   let miss = 0;
 
+  const words = ['apple', 'imagine', 'supply', 'fun', 'happy', 'air', 'sky'];
+
   target.innerText = currentWord;
   scoreLabel.innerText = score;
   missLabel.innerText = miss;
+
+  const configureTarget = () => {
+    currentWord = words[Math.floor(Math.random() * words.length)];
+    target.innerText = currentWord;
+    currentLocation = 0;
+  };
 
   window.addEventListener('keyup', e => {
     if (
@@ -27,7 +35,12 @@
         currentLocation
       )}`;
       score += 1;
+
       scoreLabel.innerText = score;
+
+      if (currentWord.length === currentLocation) {
+        configureTarget();
+      }
     } else {
       miss += 1;
       missLabel.innerText = miss;
