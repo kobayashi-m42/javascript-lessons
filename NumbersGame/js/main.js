55fd092186666f0e79b4e93fde1351ac3ef169cc
kobayashi-m42 <m42kobayashi@gmail.com>
2018-06-01 01:15:50 +0900

    JavaScriptで数値パネルを表示。

---
+++ NumbersGame/js/main.js
@@ -0,0 +1,24 @@
+(() => {
+  const board = document.getElementById('board');
+  const SIZE = 3;
+  let currentNumber = 0;
+
+  const createPanel = panelNumber => {
+    const panel = document.createElement('div');
+    panel.className = 'panel';
+    panel.textContent = panelNumber;
+
+    panel.addEventListener('click', e => {
+      if (e.target.textContent - 0 === currentNumber) {
+        e.target.className = 'panel flipped';
+        currentNumber += 1;
+      }
+    });
+
+    return panel;
+  };
+
+  for (let i = 0; i < SIZE * SIZE; i += 1) {
+    board.appendChild(createPanel(i));
+  }
+})();
