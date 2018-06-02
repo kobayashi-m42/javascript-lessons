39aa1c06ee3bbfd4cf509d39f883855366cc26e3
kobayashi-m42 <m42kobayashi@gmail.com>
2018-06-02 17:05:34 +0900

    JavasScriptでカードを動的に作成する。

---
+++ MemoryGame/js/main.js
@@ -0,0 +1,27 @@
+(() => {
+  const pairs = 4;
+
+  const createCard = num => {
+    const container = document.createElement('div');
+    container.className = 'card-container';
+
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const inner = `<div class="card-front">${num}</div><div class="card-back">?</div>`;
+
+    card.innerHTML = inner;
+    container.appendChild(card);
+
+    return container;
+  };
+
+  const initCard = () => {
+    for (let i = 1; i <= pairs; i += 1) {
+      document.getElementById('stage').appendChild(createCard(i));
+      document.getElementById('stage').appendChild(createCard(i));
+    }
+  };
+
+  initCard();
+})();
