a0257266b01b678afa2a80168667ab581e28be78
kobayashi-m42 <m42kobayashi@gmail.com>
2018-06-15 16:08:00 +0900

    停止、リセット機能を作成。

--- StopWatch/js/main.js
+++ StopWatch/js/main.js
@@ -1,12 +1,19 @@
 (() => {
   const timer = document.getElementById('timer');
   const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
 
   let startTime;
+  let elapsedTime = 0;
+  let timerId;
+  let timeToAdd = 0;
+
+  let isRunning = false;
 
   const zeroPadding = (value, digit) => `000${value}`.slice(-digit);
 
-  const updateTimerText = elapsedTime => {
+  const updateTimerText = () => {
     const m = Math.floor(elapsedTime / 60000);
     const s = Math.floor((elapsedTime % 60000) / 1000);
     const ms = elapsedTime % 1000;
@@ -17,15 +24,31 @@
   };
 
   const countUp = () => {
-    setTimeout(() => {
-      const elapsedTime = Date.now() - startTime;
+    timerId = setTimeout(() => {
+      elapsedTime = Date.now() - startTime + timeToAdd;
       updateTimerText(elapsedTime);
       countUp();
     }, 10);
   };
 
   start.addEventListener('click', () => {
+    if (isRunning === true) return;
+    isRunning = true;
     startTime = Date.now();
     countUp();
+  });
+
+  stop.addEventListener('click', () => {
+    if (isRunning === false) return;
+    isRunning = false;
+    clearTimeout(timerId);
+    timeToAdd += Date.now() - startTime;
+  });
+
+  reset.addEventListener('click', () => {
+    if (isRunning === true) return;
+    elapsedTime = 0;
+    timeToAdd = 0;
+    updateTimerText();
   });
 })();
