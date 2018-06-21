f617bc1fd7ba28bf9a6569d2d890e2f8d0a39fb5
kobayashi-m42 <m42kobayashi@gmail.com>
2018-06-21 20:26:16 +0900

    Node.jsでローカルサーバーを起動処理を追加

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+const mime = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.ico': 'image/x-icon'
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = `${__dirname}/Bingo`;
+  if (req.url === '/') {
+    filePath += '/index.html';
+  } else {
+    filePath += req.url;
+  }
+
+  fs.readFile(filePath, 'UTF-8', (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write('not found!');
+      res.end();
+    }
+    res.writeHead(200, { 'Content-Type': mime[path.extname(filePath)] });
+    res.end(data);
+  });
+});
+
+server.listen(port);
+console.log(`server start at http://localhost:${port}`);
