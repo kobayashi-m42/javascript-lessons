2716e6c8018ae653ab5116f0a5a3f30348c2546d
kobayashi-m42 <m42kobayashi@gmail.com>
2018-07-09 17:45:34 +0900

    TwitterBotの認証情報を設定しTwitterに接続する

---
+++ twitterBot.js
@@ -0,0 +1,13 @@
+const Twitter = require('twitter');
+require('dotenv').config();
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+client.get('account/verify_credentials', {}).then(content => {
+  console.log(content);
+});
