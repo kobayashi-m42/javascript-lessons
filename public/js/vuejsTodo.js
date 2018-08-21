d45f49620675da1d8f0a8f048fcbc6aea8a735f9
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-21 15:15:18 +0900

    Todoリストに完了状態を表すチェックボックスを追加

--- public/js/vuejsTodo.js
+++ public/js/vuejsTodo.js
@@ -4,7 +4,7 @@ new Vue({
     todos: [
       { id: 0, title: 'Task 0', isDone: false },
       { id: 1, title: 'Task 1', isDone: false },
-      { id: 2, title: 'Task 2', isDone: false }
+      { id: 2, title: 'Task 2', isDone: true }
     ]
   }
 });
