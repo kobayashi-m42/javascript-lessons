7cc3a17acf83cce6f2cc8c25d816fb0ce0cb62fd
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-21 16:05:00 +0900

    フォームを作成しTodoを追加する処理を追加

--- public/js/vuejsTodo.js
+++ public/js/vuejsTodo.js
@@ -1,18 +1,32 @@
 new Vue({
   el: '#app',
   data: {
-    todos: [
-      { id: 0, title: 'Task 0', isDone: false },
-      { id: 1, title: 'Task 1', isDone: false },
-      { id: 2, title: 'Task 2', isDone: true }
-    ]
+    item: '',
+    todos: []
   },
   methods: {
+    addItem: function () {
+      if(this.item.trim() === '') {
+        return;
+      }
+
+      const item = {
+        id: this.generateUniqueId(),
+
+        title: this.item,
+        isDone: false
+      };
+      this.item = '';
+      this.todos.push(item);
+    },
     deleteItem: function (index) {
       if (!confirm('are you sure?')) {
         return;
       }
       this.todos.splice(index, 1);
+    },
+    generateUniqueId: function () {
+      return `${new Date().getTime().toString(36)}-${Math.random().toString(36)}`
     }
   }
 });
