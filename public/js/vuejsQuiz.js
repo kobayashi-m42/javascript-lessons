594fe8ed92eeb8427745647f1b239d2d14f0bfe6
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-27 20:56:37 +0900

    クイズの正誤判定処理を追加

--- public/js/vuejsQuiz.js
+++ public/js/vuejsQuiz.js
@@ -1,14 +1,55 @@
+Vue.component('answer', {
+  props: ['answer','selectedAnswer','correctAnswer'],
+  template: `
+    <li
+      @click="judge(answer)"
+      v-bind:class="answerClass(answer)"
+    >
+      {{ answer }} {{ resultMessage(answer) }}
+    </li>
+  `,
+  methods: {
+    judge: function(){
+      this.$emit('judge', this.answer);
+    },
+    answerClass: function (answer) {
+      let className;
+      if (!this.correctAnswer) {
+        className = '';
+      } else {
+        className = answer === this.correctAnswer ? 'correct' : 'wrong';
+      }
+
+      return className;
+    },
+    resultMessage: function (answer) {
+      let message;
+
+      if (!this.correctAnswer) {
+        message = '';
+      } else {
+        if (answer === this.selectedAnswer) {
+          message = answer === this.correctAnswer ? ' ... CORRECT!' : ' ... WRONG!';
+        }
+      }
+
+      return message;
+    }
+  }
+});
+
 new Vue({
   el: '#app',
   data: {
-    // quizSets:
-    // { question: 'What is A?', answers: ['A0', 'A1', 'A2', 'A3'] }
     quizSets: {
       question: '',
       answer: []
-    }
+    },
+    selectedAnswer: '',
+    correctAnswer: '',
+    csrfToken: ''
   },
-  methods:{
+  methods: {
     fetchQuiz: async function (){
       try {
         const request = {
@@ -23,12 +64,42 @@ new Vue({
       } catch (error) {
         return Promise.reject(error);
       }
-    }
+    },
+    fetchAnswer: async function (selectedAnswer) {
+      try {
+        const request = {
+          method: 'post',
+          credentials: 'same-origin',
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'CSRF-Token': this.csrfToken
+          },
+          body: `selectedAnswer=${selectedAnswer}`
+        };
+        const response = await fetch('/api/quiz', request);
+        return await response.json();
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    },
+    handleAnswerClick: async function (answer){
+      if (this.selectedAnswer !== '') {
+        return;
+      }
+      this.selectedAnswer = answer;
+      try {
+        const response = await this.fetchAnswer(this.selectedAnswer);
+        this.correctAnswer = response.correctAnswer;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
   mounted: async function () {
     try {
       const response = await this.fetchQuiz();
       this.quizSets = response.currentQuiz;
+      this.csrfToken = response.csrfToken;
     } catch (error) {
       console.log(error);
     }
